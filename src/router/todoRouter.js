3df8a5d87425656397023899e8bedaf2c00e9990
DeepSeeDev <hyuk2000s@naver.com>
2025-04-01 18:18:28 +0900

    feat: react with springboot

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -10,7 +10,7 @@ const TodoRead = lazy(() => import("../pages/todo/ReadPage"))
 
 const TodoAdd = lazy(() => import("../pages/todo/AddPage"))
 
-const TodoModify = lazy(() => import("../pages/todo/Modify"))
+const TodoModify = lazy(() => import("../pages/todo/ModifyPage"))
 
 
 const todoRouter = () => {
